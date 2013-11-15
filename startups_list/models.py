5b4fa210e8992264d6232f707a14a1da8a27b251
Victor Piousbox <piousbox@gmail.com>
2013-11-15 10:50:04 -0800

    adding some models

--- startups_list/models.py
+++ startups_list/models.py
@@ -1,3 +1,32 @@
+
 from django.db import models
+from django.utils import timezone
+
+import datetime
+
 
 # Create your models here.
+
+class Startup(models.Model):
+  name = models.CharField(max_length=256)
+  website_url = models.CharField(max_length=256)
+  descr = models.TextField()
+
+  def __unicode__(self):
+    return self.name
+
+class Recruiter(models.Model):
+  name = models.CharField(max_length=256)
+  website_url = models.CharField(max_length=256)
+  descr = models.TextField()
+
+  def __unicode__(self):
+    return self.name
+
+class MediaOutlet(models.Model):
+  name = models.CharField(max_length=256)
+  website_url = models.CharField(max_length=256)
+  descr = models.TextField()
+
+  def __unicode__(self):
+    return self.name
