be9516f5c5464bdcbaa83d4efc85357343dad0e4
Victor Piousbox <piousbox@gmail.com>
2013-11-15 11:09:24 -0800

    all fields everywhere are now optional

--- startups_list/models.py
+++ startups_list/models.py
@@ -8,25 +8,25 @@ import datetime
 # Create your models here.
 
 class Startup(models.Model):
-  name = models.CharField(max_length=256)
-  website_url = models.CharField(max_length=256)
-  descr = models.TextField()
+  name = models.CharField(max_length=256, null=True, blank=True)
+  website_url = models.CharField(max_length=256, null=True, blank=True)
+  descr = models.TextField(null=True, blank=True)
 
   def __unicode__(self):
     return self.name
 
 class Recruiter(models.Model):
-  name = models.CharField(max_length=256)
-  website_url = models.CharField(max_length=256)
-  descr = models.TextField()
+  name = models.CharField(max_length=256, null=True, blank=True)
+  website_url = models.CharField(max_length=256, null=True, blank=True)
+  descr = models.TextField(null=True, blank=True)
 
   def __unicode__(self):
     return self.name
 
 class MediaOutlet(models.Model):
-  name = models.CharField(max_length=256)
-  website_url = models.CharField(max_length=256)
-  descr = models.TextField()
+  name = models.CharField(max_length=256, null=True, blank=True)
+  website_url = models.CharField(max_length=256, null=True, blank=True)
+  descr = models.TextField(null=True, blank=True)
 
   def __unicode__(self):
     return self.name
