24758a7b12809ad0c5ad0ff8e425667dd8fc1f9c
Victor Piousbox <piousbox@gmail.com>
2013-11-17 13:30:30 -0800

    startups list is displayed outside the admin.

--- startups_list/views.py
+++ startups_list/views.py
@@ -1,7 +1,11 @@
+
 from django.shortcuts import render
 
+from startups_list.models import Startup
+
 def index(request):
-  context = { 'nothing': 'nothing' }
+  startups = Startup.objects.order_by( 'name' )
+  context = { 'nothing': 'nothing', 'startups': startups }
   return render(request, 'startups/index.html', context)
 
 def sanity(request):
